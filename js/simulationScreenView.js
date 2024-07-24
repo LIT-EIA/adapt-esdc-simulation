169457cfaec69099a883729e30645e471af25d6b
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-07-24 16:14:10 -0400

    WIP controller form mode

--- js/simulationScreenView.js
+++ js/simulationScreenView.js
@@ -8,7 +8,8 @@ define([
       'click .simulation-action-element': 'handleAction',
       'change .simulation-action-element': 'handleAction',
       'input .simulation-action-element': 'handleAction',
-      'keypress .simulation-action-element': 'handleAction'
+      'keypress .simulation-action-element': 'handleAction',
+      'click .simulation-form .form-submit': 'handleSubmit'
     },
 
     initialize: function () {
@@ -16,12 +17,25 @@ define([
       ////console.log('simulation screen view this: ', this);
       var _childItems = this.model.get('_childItems')
       _childItems.forEach(function (action, index) {
-        _childItems[index].id = `screen-action-${index}`;
-        _childItems[index].type = {
+        var childIndex = index;
+        _childItems[childIndex].id = `screen-action-${childIndex}`;
+        _childItems[childIndex].type = {
           input: action._actionType === 'input',
           select: action._actionType === 'select',
           click: action._actionType === 'click'
         };
+        if (_childItems[childIndex]._isForm) {
+          _childItems[childIndex]._form.forEach(function (action, index) {
+            var formIndex = index;
+            _childItems[childIndex]._form[formIndex].id = `screen-action-${childIndex}-${formIndex}`;
+            _childItems[childIndex]._form[formIndex].type = {
+              input: action._actionType === 'input',
+              select: action._actionType === 'select',
+              submit: action._actionType === 'submit'
+            };
+          });
+        }
+
       });
       this.model.set('_childItems', _childItems);
     },
@@ -29,7 +43,7 @@ define([
 
     render: function () {
       var data = this.model.toJSON();
-      console.log(data);
+      //console.log(data);
       var template = Handlebars.templates['simulationScreen'];
       this.$el.html(template(data));
       return this;
@@ -53,7 +67,73 @@ define([
           this.handleInput(e);
           break;
         default:
-          //console.log('Unhandled event type:', eventType);
+        //console.log('Unhandled event type:', eventType);
+      }
+    },
+
+    handleSubmit: function (e) {
+      var self = this;
+      var form = $(e.target).parent();
+      var formId = $(form).attr('data-id');
+      var formModel = this.model.get('_childItems').find(item => item.id === formId);
+      var formActions = formModel._form;
+      var elements = $(form).find('input, select');
+
+      var submitId = $(e.target).attr('data-id');
+      var submitAction = formActions.find(item => item.id === submitId);
+
+      var errors = [];
+      $.each(elements, function(){
+        var actionId = $(this).attr('data-id');
+        var action = formActions.find(item => item.id === actionId);
+        var fieldValue = $(this).val();
+        if(action._actionType === 'input'){
+          var inputString = fieldValue;
+          var criteriaList = action._matchTextItems;
+          var isMatched = self.matchString(inputString, criteriaList);
+          if (!isMatched) {
+            errors.push({
+              name: action.title,
+              message: action.matchFailure,
+              userValue: fieldValue
+            });
+          }
+        } else if (action._actionType === 'select'){
+          var selectedOption = fieldValue;
+          var correctOption = action._selectOptions.filter(function (option) {
+            return option._correctOption === true
+          })[0];
+          var correctOptionValue = correctOption._selectValue;
+          if (selectedOption !== correctOptionValue) {
+            errors.push({
+              name: action.title,
+              message: action.selectFailure,
+              userValue: fieldValue
+            });
+          }
+        }
+      });
+      if(errors.length > 0){
+        var template = Handlebars.templates['simulationErrors'];
+        var messageHTML = template({errors: errors});
+        Adapt.trigger('notify:popup', {
+          title: 'Incorrect Action',
+          body: messageHTML
+        });
+      } else {
+        console.log(formModel);
+        if(formModel._isSuccess){
+          Adapt.trigger('notify:popup', {
+            title: 'Completion Message',
+            body: formModel._successBody
+          });
+        } else {
+          var eventData = {
+            id: submitAction._goTo,
+            componentID: self.model.get('componentID')
+          }
+          Adapt.trigger('simulationloadscreen', eventData);
+        }
       }
     },
 
@@ -62,10 +142,10 @@ define([
       var action = this.model.get('_childItems').find(item => item.id === actionId);
       if (action._actionType === 'click') {
         //console.log('click action:', action);
-        if (action._failure._isFailure) {
+        if (action._isFailure) {
           Adapt.trigger('notify:popup', {
             title: 'Incorrect Action',
-            body: action._failure.body
+            body: action._failureBody
           });
         } else {
           var eventData = {
@@ -82,10 +162,10 @@ define([
       var action = this.model.get('_childItems').find(item => item.id === actionId);
       if (action._actionType === 'select') {
         ////console.log('change action:', action);
-        if (action._failure._isFailure) {
+        if (action._isFailure) {
           Adapt.trigger('notify:popup', {
             title: 'Incorrect Action',
-            body: action._failure.body
+            body: action._failureBody
           });
         } else {
           //console.log('this', this);
@@ -122,10 +202,10 @@ define([
       var actionId = $(e.target).attr('data-id');
       var action = this.model.get('_childItems').find(item => item.id === actionId);
       if (action._actionType === 'input') {
-        if (action._failure._isFailure) {
+        if (action._isFailure) {
           Adapt.trigger('notify:popup', {
             title: 'Incorrect Action',
-            body: action._failure.body
+            body: action._failureBody
           });
         } else {
           ////console.log('input action:', action);
