9888d674d25277ad4a601d910e33a4cc86c675e7
petchen123 <peterchen_karwing@outlook.com>
2024-10-01 13:33:15 -0400

    correct refocus issue for show instructions prompt

--- js/simulationView.js
+++ js/simulationView.js
@@ -124,7 +124,7 @@ define([
               promptText: "OK"
             }
           ],
-          onCloseRefocusEl: $(e.target).parent()
+          onCloseRefocusEl: $(e.target)
         });
       }
     },
@@ -403,6 +403,7 @@ define([
       self.$el.find('.start-simulation').removeClass('display-none');
       self.$el.find('.simulation-graphic').removeClass('sticky-margin');
       self.$el.find('.simulation-graphic img').addClass('simulation-disabled');
+      self.$el.find('.start-simulation').focus();
     }
 
   });
