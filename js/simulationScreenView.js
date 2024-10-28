ecb875a6f83f7c27700a4eba2e8ff50a5a0b7cb8
petchen123 <peterchen_karwing@outlook.com>
2024-10-28 07:47:56 -0400

    use _failureBody instead of matchFailure for checkbox

--- js/simulationScreenView.js
+++ js/simulationScreenView.js
@@ -400,7 +400,7 @@ define([
             self.handleConfirmSuccess(componentID);
           });
         } else if (action._isFailure) {
-          var failureBody = action.matchFailure ? action.matchFailure : this.model.get('incorrectFallback');
+          var failureBody = action._failureBody ? action._failureBody : this.model.get('incorrectFallback');
           Adapt.trigger('simulation-notify:prompt', {
             body: failureBody,
             _prompts: [
